9883d1a562380a71faea32be1bb15d11f9a47fa6
Ohyeahchacarron <rp6245397@gmail.com>
2025-08-03 17:08:43 -0600

    proteccion de usuarios

--- HidroSense/Controllers/UsuarioController.cs
+++ HidroSense/Controllers/UsuarioController.cs
@@ -24,7 +24,6 @@ public class UsuariosController : ControllerBase
     }
 
     [HttpGet("getUsuarios")]
-    [AllowAnonymous]
     public async Task<ActionResult<List<Usuario>>> getUsuarios()
     {
 
